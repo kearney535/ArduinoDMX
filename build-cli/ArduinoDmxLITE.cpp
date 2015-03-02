a079108fb85a0c31853a4d0beb59a17dce7a0390
Sam Kearney <samuelmkearney@gmail.com>
2015-03-02 02:36:28 +0000

    Modify to add automatic 50ms fade

--- build-cli/ArduinoDmxLITE.cpp
+++ build-cli/ArduinoDmxLITE.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-//#include "C:\Program Files (x86)\Arduino\libraries\Arduino-IRremote-master\IRremote.h"
-
 #include <IRremote.h>
 
 #include <TimerOne.h>
@@ -11,13 +9,25 @@
 
 #define DMX_OUT_PIN 3
 #define IR_PIN 4
+#define NUM_OF_CHANNELS 16
+
+#define FADE_STEP 10
+#define FADE_TIME 5
+
+uint8_t fadeCounter = 0;
 
 uint8_t value = 0;
 uint8_t IRvalue = 0;
 uint16_t channel;
 
+float channel_values[NUM_OF_CHANNELS + 1];
+float channel_steps[NUM_OF_CHANNELS + 1];
+
 IRrecv irrecv(IR_PIN);
 decode_results results;
+
+void fade(uint16_t channel, uint8_t value);
+void increment();
 
 /*
   COMMAND SYNTAX
@@ -30,6 +40,9 @@ void setup() {
   // Initialize globals
   value = 0;
 
+  memset(channel_values, 0, sizeof(channel_values));
+  memset(channel_steps, 0, sizeof(channel_steps));
+
   // Initialize serial
   Serial.begin(115200);
 
@@ -37,7 +50,10 @@ void setup() {
   DmxSimple.usePin(3);
 
   irrecv.enableIRIn();
+
+  Timer1.initialize(1000 * FADE_STEP);
 }
+
 
 void loop() {
   int c;
@@ -53,7 +69,8 @@ void loop() {
       else if (c=='w') {
         if (channel > 0)
         {
-          DmxSimple.write(channel, value);
+          fade(channel, value);
+       //   DmxSimple.write(channel, value);
         }
       }
       else if (c == 'q')
@@ -73,4 +90,35 @@ void loop() {
 //    Serial.println("Resuming");
     irrecv.resume();
   }
+}
+
+void fade(uint16_t channel, uint8_t value)
+{
+    fadeCounter = 0;
+    channel_steps[channel] = ((float)value - (float)channel_values[channel])/(float)FADE_TIME;
+    Serial.print(channel_steps[channel]);
+    Timer1.attachInterrupt(increment);
+}
+
+void increment()
+{
+    for (uint8_t i = 0; i < NUM_OF_CHANNELS; i++)
+    {
+        if (channel_steps[i] != 0)
+        {
+            channel_values[i] += channel_steps[i];
+            DmxSimple.write(i, (uint8_t)(channel_values[i]));
+        }
+    }
+    fadeCounter++;
+    if (fadeCounter == FADE_TIME)
+    {
+        Timer1.detachInterrupt();
+        for (uint8_t i = 0; i < NUM_OF_CHANNELS; i++)
+        {
+            channel_steps[i] = 0;
+            channel_values[i] = round(channel_values[i]);
+        }
+        fadeCounter = 0;
+    }
 }
